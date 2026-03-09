5bbc8ededba02ac995e12ac62d312825b5f051c7
Roy Asraf <117677549+roy-asraf1@users.noreply.github.com>
2026-03-09 12:48:05 +0200

    Add PowerShell script for webhook subscription setup

---
+++ webhook.ps1
@@ -0,0 +1,16 @@
+#connection to the system
+Import-Module "C:\Program Files\Quest\ChangeAuditor\Client\ChangeAuditor.PowerShell.dll"
+$connection = Connect-CAClient
+
+#configuration
+$notificationUrl = "https:/domain/api/webhook"
+
+#what systems
+#$selectedSubsystems = Get-CAEventExportSubsystems -Connection $connection
+$selectedSubsystems = Get-CAEventExportSubsystems -Connection $connection | Where-Object DisplayName -In -Value "Active Directory", "Registry", "File System", "Change Auditor", "Exchange", "Group Policy", "Service", "Computer", "Local Account", "SQL", "ADAM (AD LDS)", "AD Query", "VMware", "SharePoint", "Logon Activity", "Microsoft 365", "Microsoft Entra", "Threat Detection", "Active Directory Federation Services", "SQL Extended Events"
+New-CAEventWebhookSubscription -Connection $connection -NotificationUrl $notificationUrl -Subsystems $selectedSubsystems -BatchSize 10000
+
+
+
+Get-CAEventWebhookSubscriptions -Connection $connection | Format-List
+Get-CAEventExportSubsystems -Connection $
